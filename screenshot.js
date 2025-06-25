b3da3b8da59fd33f41c0e9042b0d672522e07527
Marco Kronenberg <marco.kronenberg@bafu.admin.ch>
2025-06-25 14:53:18 +0200

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -6,7 +6,7 @@ const path = require('path');
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
   await page.goto('https://visualize.admin.ch/de/v/4lPHCdjPS8Se', { waitUntil: 'networkidle2' });
-  await new Promise(resolve => setTimeout(resolve, 2000));
+  await new Promise(resolve => setTimeout(resolve, 5000));
   const dir = path.join(__dirname, 'screenshots');
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
