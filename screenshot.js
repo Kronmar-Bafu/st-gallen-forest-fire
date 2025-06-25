61d8e4aeb096d132d234731823a64be328032214
Marco Kronenberg <marco.kronenberg@bafu.admin.ch>
2025-06-25 12:04:37 +0200

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 (async () => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
-  await page.goto('https://yourhomepage.com', { waitUntil: 'networkidle2' });
+  await page.goto('https://visualize.admin.ch/de/v/4lPHCdjPS8Se', { waitUntil: 'networkidle2' });
   await page.screenshot({ path: 'screenshot.png', fullPage: true });
   await browser.close();
 })();
