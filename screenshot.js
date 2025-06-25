b93324e277c8e30c092278815ae70aa089528a33
Marco Kronenberg <marco.kronenberg@bafu.admin.ch>
2025-06-25 13:46:58 +0200

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const path = require('path');
 
 (async () => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
